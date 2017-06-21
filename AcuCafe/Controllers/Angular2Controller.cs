53efff2b4a95b4b9bfbc560a9fa23f6ddb380395
hurdissej <hurdissej@me.com>
2017-06-21 15:47:09 +0100

    Angular quickstart working

---
+++ AcuCafe/Controllers/Angular2Controller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AcuCafe.Controllers
+{
+    public class Angular2Controller : Controller
+    {
+        // GET: Angular2
+        public ActionResult Index()
+        {
+            return View("~/clientapp/src/index.cshtml");
+        }
+    }
+}
